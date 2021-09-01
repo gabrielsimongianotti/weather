891cd248068bcf9868b34f8bac19fcae8a924a32
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-09-01 15:15:45 -0300

    feat: start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+function App () {
+  const [page, setPage] = useState(2)
+  const items = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 25],
+    [26, 27, 28, 29, 30],
+    [21, 22, 23, 24, 25],
+    [26, 27, 28, 29, 30],
+    [21, 22, 23, 24, 25],
+    [26, 27, 28, 29, 30]
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {items[page].map(item => (<p>{item}</p>))}
+      {Array.from({ length: items.length }).map((item, index) => {
+        if (index+2 >= page && page + 5 > index) {
+          return (<p onClick={()=>{setPage(index)}}>{index+1 }</p>)
+        }
+      })}
     </div>
   );
 }
