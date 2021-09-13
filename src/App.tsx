b82d187723d2697ab29231a6873e7180015552b1
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-09-13 05:30:49 -0300

    feat: add apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { ApolloProvider } from '@apollo/client';
 
+import apolloClient from './services/apollo';
 import { Routes } from './Routes';
+import { GlobalStyle } from "./styles/global";
 
 function App () {
   return (
-    <>
+    <ApolloProvider client={apolloClient}>
+      <GlobalStyle />
       <Routes />
-    </>
+    </ApolloProvider>
   );
 }
 
